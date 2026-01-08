a02288dc0ad027494bafbfc366dcbbaaaa6b9895
Franck andritina <rakotomavofranck007@gmail.com>
2026-01-08 18:06:42 +0300

    first commit on main

--- shop/admin.py
+++ shop/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Category, Product, CustomerProfile, Order, OrderItem, CartItem, WishlistItem
 
 # Register your models here.
+# admin.site.site_header = "Biscuit Shop Administration"
+# admin.site.site_title = "Biscuit Shop Portal"
+# admin.site.index_title = "Welcome to the Bakery Management"
 
 admin.site.register(Category)
 admin.site.register(Product)

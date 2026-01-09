559880e3430210da49db77a9709d98264af77e65
Franck andritina <rakotomavofranck007@gmail.com>
2026-01-09 14:46:40 +0300

    admin site completely personnalized

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from .models import Category, Product, CustomerProfile, Order, OrderItem, CartItem, WishlistItem
 from django.http import HttpResponse
 from django.db.models import Sum
@@ -18,6 +20,22 @@ class BiscuitAdminSite(admin.AdminSite):
         context['stats_order_count'] = order_count
         
         return context
+    
+    def get_app_list(self, request, app_label=None):
+        app_dict = self._build_app_dict(request)
+        app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
+
+        for app in app_list:
+            if app['app_label'] == 'shop':
+                ordering = {
+                    'Order': 1,
+                    'Product': 2,
+                    'Category': 3,
+                    'CustomerProfile': 4,
+                }
+                app['models'].sort(key=lambda x: ordering.get(x['object_name'], 99))
+
+        return app_list
 
 admin_site = BiscuitAdminSite(name='myadmin')
 
@@ -47,7 +65,6 @@ class PriceRangeFilter(admin.SimpleListFilter):
         if self.value() == 'high':
             return queryset.filter(price__gt=5)
 
-@admin.action(description='Export selection in CSV')
 def export_as_csv(modeladmin, request, queryset):
     
     response = HttpResponse(content_type='text/csv')
@@ -65,14 +82,12 @@ def export_as_csv(modeladmin, request, queryset):
         
     return response
 
-@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     list_editable = ('description',)
     search_fields = ('name',)
     inlines = [ProductInline]
 
-@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock', 'category')
     list_editable = ('price', 'stock')
@@ -82,25 +97,21 @@ class ProductAdmin(admin.ModelAdmin):
     actions = [export_as_csv]
     list_filter = (PriceRangeFilter, 'category',  'stock')
 
-@admin.register(CustomerProfile)
 class CustomerProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'phone_number', 'address')
     search_fields = ('user__username', 'phone_number')
     autocomplete_fields = ('user',)
 
-@admin.register(WishlistItem)
 class WishlistItemAdmin(admin.ModelAdmin):
     list_display = ('user', 'product', 'added_at')
     search_fields = ('user__username', 'product__name')
     autocomplete_fields = ('user', 'product')
 
-@admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ('user', 'product', 'quantity')
     search_fields = ('user__username', 'product__name')
     autocomplete_fields = ('user', 'product')
     
-@admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('order', 'product', 'quantity', 'price')
     list_filter = ('order',)
@@ -112,7 +123,6 @@ class OrderItemInline(admin.TabularInline):
     extra = 1
     fields = ('product', 'quantity', 'price')
 
-@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'status', 'created_at', 'get_total')
     list_editable = ('status',)
@@ -129,3 +139,15 @@ class OrderAdmin(admin.ModelAdmin):
         queryset = super().get_queryset(request)
         return queryset.prefetch_related('items')
     
+
+#model registration with the personnalized admin site
+admin_site.register(User, UserAdmin)
+admin_site.register(Group, GroupAdmin)
+admin_site.register(Category, CategoryAdmin)
+admin_site.register(Product, ProductAdmin)
+admin_site.register(CustomerProfile, CustomerProfileAdmin)
+admin_site.register(Order, OrderAdmin)
+admin_site.register(OrderItem, OrderItemAdmin)
+admin_site.register(CartItem, CartItemAdmin)
+admin_site.register(WishlistItem, WishlistItemAdmin)
+admin_site.add_action(export_as_csv)

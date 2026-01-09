ad94b0c01bd2cbb5240ea8d4814a3072aeb94901
Franck andritina <rakotomavofranck007@gmail.com>
2026-01-09 14:46:40 +0300

    admin site completely personnalized

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from decimal import Decimal
+from django.urls import reverse
 
 #Category model to classify biscuits
 class Category(models.Model):
@@ -21,6 +21,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
+    
+    def get_absolute_url(self):
+        return reverse('product-detail', args=[self.id]) #type: ignore
 
     def __str__(self):
         return self.name

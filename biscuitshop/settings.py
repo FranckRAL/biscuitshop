25f5c553bc28b6f9cf5ce0cb5261abe51be47788
Franck andritina <rakotomavofranck007@gmail.com>
2026-01-23 19:40:29 +0300

    keeping the test in the sqlite3 and prod in neon

--- biscuitshop/settings.py
+++ biscuitshop/settings.py
@@ -128,6 +128,8 @@ WSGI_APPLICATION = 'biscuitshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': dj_database_url.config(
         default= env('DATABASE_URL'),
@@ -135,6 +137,12 @@ DATABASES = {
         ssl_require=True 
     )
 }
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 
 

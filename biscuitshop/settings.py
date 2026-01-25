db795d317f169faa5e8dee6959f1df649849c6d9
Franck andritina <rakotomavofranck007@gmail.com>
2026-01-25 18:25:17 +0300

    changing database setup order

--- biscuitshop/settings.py
+++ biscuitshop/settings.py
@@ -134,6 +134,12 @@ WSGI_APPLICATION = 'biscuitshop.wsgi.application'
 
 
 
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
 DATABASES = {
     'default': dj_database_url.config(
         default= env('DATABASE_URL'),
@@ -141,15 +147,6 @@ DATABASES = {
         ssl_require=True 
     )
 }
-
-
-if 'test' in sys.argv:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
